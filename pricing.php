9096dcaf3d9a338a4a22ec5ce8d1210085185bb4
titonugroho15 <titonugroho15@gmail.com>
2018-01-08 15:58:54 +0700

    tambahpricing

---
+++ pricing.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    	<title>Agan Hotel</title>
+    <link href="stylesheet.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="css/grid.css" type="text/css" media="screen" />   
+    <script src="js/jquery-1.6.2.min.js" type="text/javascript"></script>    
+	<!--[if lt IE 7]>
+        <div style=' clear: both; text-align:center; position: relative;'>
+            <a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0"  alt="" /></a>
+        </div>
+	<![endif]-->
+    <!--[if lt IE 9]>
+   		<script type="text/javascript" src="js/html5.js"></script>
+        <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+	<![endif]-->
+</head>
+<body>
+    <div class="main_container" >
+        <a href="index.php"><img src="images/logo.png" align="left" /></a>
+            <div id="headr_img">
+                <span id="main_nav"> 
+                	   	<a href="index.php">Menu Utama</a> |  
+						<a href="catalogue.php">Akomodasi</a> |  
+                        <a href="reserve.php">Reservasi</a> |  
+                        <a href="contact.php">Contact Person</a>
+	            </span>
+            </div>
+    
+<!-- content -->
+    <section id="content">
+        <div class="bg-top">
+        	<div class="bg-top-2">
+                <div class="bg">
+                    <div class="bg-top-shadow">
+                        <div class="main">
+                        <h2 id="home_header">Harga Kamar</h2><br/>
+                            <div class="box">
+                                <div class="padding">
+                                    <div class="container_12">
+                                        <div class="wrapper">
+                                            <div class="grid_12">
+                                            	<div class="indent-left p2">
+                                                </div>
+                                                <div class="wrapper p4">
+                                                    <article class="grid_4 alpha">
+                                                       <div class="indent-left">
+                                                         <figure class="frame2 p2"><img src="images2/stan1.jpg" alt="" /></figure>
+                                                           <div class="title t2 img-indent3">Standard</div>
+                                                            Cocok untuk dua (2) orang, dilengkapi dengan fasilitas AC, TV Cable, and Bak air panas dan dingin. 
+                                                            <div class="wrapper">
+                                                            	<span class="price fleft">Rp. 700.000</span>
+                                                                <a class="button fright" href="standard.php">Gallery</a>
+                                                            </div>
+                                                        </div>
+                                                    </article>
+                                                    <article class="grid_4">
+                                                        <div class="indent3">
+                                                            <figure class="frame2 p2"><img src="images2/del1.jpg" alt="" /></figure>
+                                                            <p class="title t2 img-indent3">Sedang</p>
+                                                            Cocok untuk tiga (3) orang, dilengkapi dengan fasilitas AC, TV Cable, and Bak air panas dan dingin. 
+                                                            <div class="wrapper">
+                                                            	<span class="price fleft">Rp. 800.000</span>
+                                                                <a class="button fright" href="deluxe.php">Gallery</a>
+                                                            </div>
+                                                        </div>
+                                                    </article>
+                                                    <article class="grid_4 omega">
+                                                        <div class="indent-right">
+                                                            <figure class="frame2 p2"><img src="images2/fam1.jpg" alt="" /></figure>
+                                                            <p class="title t2 img-indent3">Keluarga</p>
+                                                            Cocok Untuk empat(4) orang, dilengkapi dengan fasilitas AC, TV Cable, and Bak air panas dan dingin. 
+                                                            <div class="wrapper">
+                                                            	<span class="price fleft">Rp. 900.000</span>
+                                                                <a class="button fright" href="family.php">Gallery</a>
+                                                            </div>
+                                                        </div>
+                                                    </article>
+                                                </div>                                           
+                    	                           <article class="grid_4 alpha">
+                        	                                <div class="wrapper p4">
+                                                               <div class="indent-left">
+                            	                               <figure class="img-indent frame2"><img src="images2/wifi-zone.jpg" alt="" /></figure>
+                                                               </div>
+                                                            </div>
+                                                    </article> 
+                                                    <article class="grid_8 alpha">
+                        	                           <h4 class="prev-indent-bot2">Fasilitas Tambahan</h4>
+                                                            <div class="wrapper">
+                            	                               <article class="grid_4 alpha">
+                                	                               <div class="indent-right2">
+                                                                        <ul class="price-list">
+                                                                            <li><span>Rp. 200.000</span>Tambahan Kasur<strong>&nbsp;</strong></li>
+                                                                            <li><span>Rp.  50.000</span>Tambahan Handuk<strong>&nbsp;</strong></li>
+                                                                            <li><span>Rp.  30.000</span>Tambahan Bantal<strong>&nbsp;</strong></li>
+                                                                            <li class="last"><span>Rp. 20.000</span>Tambahan selimut<strong>&nbsp;</strong></li>
+                                                                        </ul>
+                                                                    </div>
+                                                                </article>
+                                                         <!--   <article class="grid_4 omega">
+                                	                           <div class="indent-right2">
+                                                                    <ul class="price-list">
+                                                                        <li><span>$6700.00</span><a href="#">Totam rem aperiam eaque</a><strong>&nbsp;</strong></li>
+                                                                        <li><span>$4370.00</span><a href="#">Inventore veritatis quasi</a><strong>&nbsp;</strong></li>
+                                                                        <li><span>$5780.00</span><a href="#">Nemo enim ipsam voluptatem</a><strong>&nbsp;</strong></li>
+                                                                        <li><span>$2300.00</span><a href="#">Voluptas sit aspernatur aut</a><strong>&nbsp;</strong></li>
+                                                                        <li><span>$3650.00</span><a href="#">Sed quia consequuntur magni</a><strong>&nbsp;</strong></li>
+                                                                        <li class="last"><span>$7900.00</span><a href="#">Ratione voluptatem sequi</a><strong>&nbsp;</strong></li>
+                                                                    </ul>
+                                                                </div>
+                                                            </article> -->
+                                                        </div>
+                                                </article>
+                                             </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>	
+            </div>
+       </div>
+    </section>
+   <div id="footer_container">
+         	Copyright © 2015 Kelompok 5
+    </div>
+</div>
+</body>
+</html>
+
